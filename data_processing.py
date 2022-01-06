2bf90e6dd80422881e38c92cb962fe3387d23ea6
Michal_Spinczyk <michal.spinczyk@gmail.com>
2022-01-06 18:17:52 +0100

    Implementation of the first functions for SP-CS method. Add new file - data_processing.py with Point class.

---
+++ data_processing.py
@@ -0,0 +1,9 @@
+# Tu powninny się znaleźć takie funkcje jak
+# * wczytywanie danych z excela
+# * wzynacznie zbiorów A0, A1, A2, A3
+from copy import deepcopy
+
+class Point:
+    def __init__(self, *cor: list):
+        self.cor = deepcopy(cor)
+        self.len = len(cor)
